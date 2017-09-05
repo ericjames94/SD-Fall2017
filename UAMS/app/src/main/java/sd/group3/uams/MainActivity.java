1709d7cf8d90462498f96da844b6c227ea74b393
Eric James <erickylejames@gmail.com>
2017-09-05 17:27:47 -0400

    Second Activity Test
    Added second activity, Activity2, and added button to MainActivity that starts Activity2

--- UAMS/app/src/main/java/sd/group3/uams/MainActivity.java
+++ UAMS/app/src/main/java/sd/group3/uams/MainActivity.java
@@ -1,7 +1,11 @@
 package sd.group3.uams;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Activity2.class));
+            }
+        });
+
     }
 }
