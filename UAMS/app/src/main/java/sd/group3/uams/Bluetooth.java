edc81e86f7b9c878ea71dee30382458a0a5e995e
Boris Lam <boris.lam@knights.ucf.edu>
2017-10-07 17:34:51 -0400

    BT
    Adding bluetooth page to app

--- UAMS/app/src/main/java/sd/group3/uams/Bluetooth.java
+++ UAMS/app/src/main/java/sd/group3/uams/Bluetooth.java
@@ -18,19 +18,58 @@ import java.util.Set;
 
 /**
  * Created by borislam on 10/7/17.
+ * Bluetooth Class - To connect UAMS and retrieve data
  */
 
 public class Bluetooth extends Fragment {
-    Button enableBluetooth, disableBluetooth, deviceList;
+    Button enableConnection, disableConnection, deviceList;
     private BluetoothAdapter BA;
     private Set<BluetoothDevice> pairedDevices;
-
     ListView lv;
 
-    @Override
     protected void onCreateView(Bundle savedInstanceState){
+        super.onCreate((savedInstanceState));
 
+        BA = BluetoothAdapter.getDefaultAdapter();
+        lv = (ListView) lv.findViewById(R.id.PairedDeviceList1);
 
+        enableConnection = (Button) enableConnection.findViewById(R.id.EnableButton);
+        disableConnection = (Button) disableConnection.findViewById(R.id.DisableButton);
+        deviceList = (Button) deviceList.findViewById(R.id.DeviceListButton);
+    }
+
+    //Enable Bluetooth Connection
+    public void enable(View v){
+        if(!BA.isEnabled()){
+            Intent bluetoothOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(bluetoothOn, 0);
+            Intent discoverable = new Intent(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+            startActivityForResult(discoverable, 0);
+            Toast.makeText(getActivity(), "Enabled Bluetooth Connection", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getActivity(), "Bluetooth is already enabled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Disable Bluetooth Connection
+    public void disable(View v){
+        BA.disable();
+        Toast.makeText(getActivity(), "Disabled Bluetooth Connection", Toast.LENGTH_SHORT).show();
+    }
+
+    //Paired List View
+    public void list(View v){
+        pairedDevices = BA.getBondedDevices();
+
+        ArrayList list = new ArrayList();
+
+        for(BluetoothDevice bd : pairedDevices){
+            list.add(bd.getName());
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, list);
+
+        lv.setAdapter(adapter);
     }
 
 }
