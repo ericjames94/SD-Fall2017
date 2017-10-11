b06063a6641aba472ffbd9a26a07bc062cef2779
Boris Lam <boris.lam@knights.ucf.edu>
2017-10-10 20:05:50 -0400

    Bluetooth
    Committing final Bluetooth changes.

--- UAMS/app/src/main/java/sd/group3/uams/Bluetooth.java
+++ UAMS/app/src/main/java/sd/group3/uams/Bluetooth.java
@@ -1,10 +1,8 @@
 package sd.group3.uams;
 
-<<<<<<< HEAD
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 
-import android.content.ComponentName;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,27 +12,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 
-=======
-//Libraries needed to utilize Fragments
-import android.support.v4.app.Fragment;
-
-//Libraries needed for Bluetooth functionality
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-
-//Misc Libraries for desired functionality
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
->>>>>>> 783858a3371846e13835f5c5ab5df46c4e94abb7
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
-import java.util.ArrayList;
+
 import java.util.Set;
 
 /**
@@ -63,7 +44,6 @@ public class Bluetooth extends Fragment {
     }
 
     @Override
-<<<<<<< HEAD
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
@@ -79,7 +59,7 @@ public class Bluetooth extends Fragment {
     public void buttonListener(){
         enableConnection.setOnClickListener(new View.OnClickListener() {
             @Override
-                public void onClick(View v) {
+            public void onClick(View v) {
                 enable(v);
             }
         });
@@ -125,19 +105,3 @@ public class Bluetooth extends Fragment {
         startActivityForResult(getVisible, 0);
     }
 }
-=======
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                                @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_bluetooth, container, false);
-
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        //you can set the title for your toolbar here for different fragments different titles
-        getActivity().setTitle("Receive Data");
-    }
-}
->>>>>>> 783858a3371846e13835f5c5ab5df46c4e94abb7
